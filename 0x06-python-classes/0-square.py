76ef9a3aed112b435f45a2dea5f73e7dccb5e696
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2023-12-20 01:36:22 +0100

    first commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is an empty class Square that defines a square
+"""
+
+class Square:
+    """
+    This is a class of square
+    Attributes:
+        Attribute not present
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Private instance attribute"""
+class Square:
+    """define function Square """
+    def __init__(self, size):
+        """Square instance with size 3"""
+        self.__size = size
