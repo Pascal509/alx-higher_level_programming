04e4059ee67663cb7a9f6b431d8fc85ddea61fc8
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2023-12-27 20:39:19 +0100

    second commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,15 +2,15 @@
 
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    add_values = add(a, b)
+    sub_values = sub(a, b)
+    mul_values = mul(a, b)
+    div_values = div(a, b)
 
-add_values = add(a, b)
-sub_values = sub(a, b)
-mul_values = mul(a, b)
-div_values = div(a, b)
-
-print("{:d} +i {:d} = {:d}".format(a, b, add_values))
-print("{:d} - {:d} = {:d}".format(a, b, sub_values))
-print("{:d} * {:d} = {:d}".format(a, b, mul_values))
-print("{:d} / {:d} = {:d}".format(a, b, div_values))
+    print("{:d} + {:d} = {:d}".format(a, b, add_values))
+    print("{:d} - {:d} = {:d}".format(a, b, sub_values))
+    print("{:d} * {:d} = {:d}".format(a, b, mul_values))
+    print("{:d} / {:d} = {:d}".format(a, b, div_values))
