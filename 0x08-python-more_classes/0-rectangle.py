983ec795b445efeb490021188d09d55405698a06
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-01-07 05:23:13 +0100

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class Rectangle:
+    """
+    Rectangle Class represents a basic Rectangle. It serves as a placeholder
+    and does not have any specific attributes or methods defined.
+    """
     pass
