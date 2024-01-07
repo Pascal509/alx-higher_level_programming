354ce4cb623d5a1336a2722107b116c44080274e
root <root@Itz-klasic101>
2024-01-07 23:30:18 +0100

    m

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
+"""
+Docstring defines a module for the functions
 
-    #Check if a is not integer or a float
+Return: an integer
+"""
+
+def add_integer(a, b=98):
+    """
+    Args:
+    a is an integer
+    b is an integer
+
+    Return:
+
+    Raises:
+    TypeError: raised with an error
+
+    """
     if not isinstance (a, (int, float)):
         raise TypeError("a must be an integer")
 
@@ -13,6 +28,3 @@ def add_integer(a, b=98):
     sum = a + b
 
     return sum
-
-import doctest
-doctest.testfile("0-add_integer.txt")
