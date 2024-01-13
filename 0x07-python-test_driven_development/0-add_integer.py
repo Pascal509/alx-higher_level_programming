284a365f880c887d0e387ea0b11877e4f8e0285d
root <root@Itz-klasic101>
2024-01-13 11:38:22 +0100

    m

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ Docstring defines a module for the functions
 Return: an integer
 """
 
+
 def add_integer(a, b=98):
     """
     Args:
@@ -17,10 +18,10 @@ def add_integer(a, b=98):
     TypeError: raised with an error
 
     """
-    if not isinstance (a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    if not isinstance (b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     a = int(a)
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Define say_my_name module
+
+"""
+
+def say_my_name(first_name, last_name=""):
+    """function prints My name"""
+
