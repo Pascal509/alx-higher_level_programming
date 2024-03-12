0a69ace105df50e36081af2777b2e3593637d2d9
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-03-12 21:25:25 +0100

    task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-  w;
-  h;
-
-  constructor(w, h) {
-    this.w = width;
-    this.h = height;
-  }
-
-  if (w <= 0 || h<= 0) {
-  return {};
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 
