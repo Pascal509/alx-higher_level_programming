4bf90f819bc08bd0befd5f27d5dfb0c9a4ed0630
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-03-12 21:11:48 +0100

    second commit

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  w;
+  h;
+
+  constructor(w, h) {
+    this.w = width;
+    this.h = height;
+  }
+
+  if (w <= 0 || h<= 0) {
+  return {};
+  }
+}
+
+module.exports = Rectangle;
