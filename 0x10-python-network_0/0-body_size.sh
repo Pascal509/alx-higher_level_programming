2c613fc4140e0d4001501678d54cd7e94ab76edd
Pascal509 <chinemeremezenagu@gmail.com>
2024-03-30 15:59:39 +0100

    second commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script takes in a URL and displays the size of the body of the response
-curl -sI 'https://reqbin.com/echo' | grep -i Content-Length
+curl -sI 'http://0.0.0.0:5000' | grep -i Content-Length | awk '{print $2}'
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that takes in a URL, sends a GET request, and displays the body of the response
+curl -s -o response.txt -w "%{http_code}" 'http://localhost:5000/route_1' | { read -r status_code && [ "$status_code" -eq 200 ] && cat response.txt || echo "Non-200 status code: $status_code";}
